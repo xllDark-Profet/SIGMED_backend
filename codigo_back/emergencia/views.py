b832fba13a0568c28e864f68f9950b7ea3ad5a98
Sebastian Sandoval <sebastian-sandoval@javeriana.edu.co>
2024-04-30 16:35:35 -0500

    Actualizacion de solicitudes y tamaño de la identificacion

--- codigo_back/emergencia/views.py
+++ codigo_back/emergencia/views.py
@@ -1,4 +1,6 @@
 import json, os
+from datetime import datetime
+from datetime import date
 from django.http import JsonResponse
 from django.views import View
 from django.utils.decorators import method_decorator
@@ -28,23 +30,30 @@ class SolicitudesView(View):
             return None
         
     def mostrar_solicitud(self, solicitud):
+        fecha_hora = solicitud.fecha_hora.strftime('%d/%m/%Y %H:%M')
         return {
-            'id': solicitud.id,
-            'emergencia_detectada': solicitud.emergencia_detectada,
-            'latitud': solicitud.latitud,
-            'longitud': solicitud.longitud,
-            'usuario': {
-                'id': solicitud.usuario.id,
-                'correo': solicitud.usuario.user.email,
-                'username': solicitud.usuario.user.username,
-            },
-            'hospital': {
-                'id': solicitud.hospital.id,
-                'nombre': solicitud.hospital.nombre
-            },
-            'triage': solicitud.triage,
-            'sintomas_presentes': solicitud.sintomas_presentes
-        }
+        'id': solicitud.id,
+        'emergencia_detectada': solicitud.emergencia_detectada,
+        'direccion': solicitud.direccion,
+        'nombre_usuario': solicitud.usuario.user.first_name,
+        'apellido_usuario': solicitud.usuario.user.last_name,
+        'correo_usuario': solicitud.usuario.user.email,
+        'fecha_nacimiento_usuario': solicitud.usuario.fecha_nacimiento,
+        'telefono_usuario': solicitud.usuario.telefono,
+        'triage': solicitud.triage,
+        'sintomas_presentes': solicitud.sintomas_presentes,
+        'nombre_hospital': solicitud.hospital.nombre,
+        'fecha_hora': fecha_hora
+    }
+        
+    def solicitud_simplificada(self, solicitud):
+        return {
+        'emergencia_detectada': solicitud.emergencia_detectada,
+        'triage': solicitud.triage,
+        'nombre_usuario': solicitud.usuario.user.first_name,
+        'apellido_usuario': solicitud.usuario.user.last_name,
+        'edad_usuario': (date.today() - solicitud.usuario.fecha_nacimiento).days // 365 if solicitud.usuario.fecha_nacimiento else None,
+    }
     
     def get(self, request):
         data = {}
@@ -54,7 +63,9 @@ class SolicitudesView(View):
         except json.decoder.JSONDecodeError:
             especifica = False
         
-        if especifica:
+        simplificada = data.get('simplificada')
+        
+        if especifica and not simplificada:
             id = data.get('id')
             
             if id:
@@ -71,7 +82,10 @@ class SolicitudesView(View):
         solicitudes_list = []
         
         for solicitud in solicitudes:
-            s = self.mostrar_solicitud(solicitud)
+            if simplificada == "si":
+                s = self.solicitud_simplificada(solicitud)
+            else:
+                s = self.mostrar_solicitud(solicitud)
             solicitudes_list.append(s)
         
         if not solicitudes_list:
@@ -84,12 +98,10 @@ class SolicitudesView(View):
             data = json.loads(request.body)
             solicitud_dto = SolicitudDTO(**data)
 
-            if not all([solicitud_dto.latitud, solicitud_dto.longitud, solicitud_dto.id_usuario, solicitud_dto.emergencia_detectada, solicitud_dto.sintomas_presentes, solicitud_dto.id_hospital, solicitud_dto.triage]):
+            if not all([solicitud_dto.direccion, solicitud_dto.id_usuario, solicitud_dto.emergencia_detectada, solicitud_dto.sintomas_presentes, solicitud_dto.id_hospital, solicitud_dto.triage]):
                 parametros_faltantes = []
-                if not solicitud_dto.latitud:
-                    parametros_faltantes.append('latitud')
-                if not solicitud_dto.longitud:
-                    parametros_faltantes.append('longitud')
+                if not solicitud_dto.direccion:
+                    parametros_faltantes.append('direccion')
                 if not solicitud_dto.id_usuario:
                     parametros_faltantes.append('id_usuario')
                 if not solicitud_dto.emergencia_detectada:
@@ -109,19 +121,27 @@ class SolicitudesView(View):
             except Usuario.DoesNotExist:
                 return JsonResponse({'mensaje': 'No existe un usuario con ese id.'}, status=404)
             
+            if usuario.tipo_usuario.lower() != "usuario":
+                return JsonResponse({'mensaje': 'No es un usuario valido para una solicitud (solo tipo usuario).'}, status=400)
+            
+            if solicitud_dto.triage < 1 or solicitud_dto.triage > 5:
+                return JsonResponse({'mensaje': "Nivel de triage invalido"}, status=400)
+            
             try:
                 hospital = Hospital.objects.get(id=solicitud_dto.id_hospital)
-            except Usuario.DoesNotExist:
+            except Hospital.DoesNotExist:
                 return JsonResponse({'mensaje': 'No existe un hospital con ese id.'}, status=404)
+            
+            fecha_hora = datetime.now()
 
             solicitud = Solicitud.objects.create(
-                latitud=solicitud_dto.latitud,
-                longitud=solicitud_dto.longitud,
+                direccion=solicitud_dto.direccion,
                 usuario=usuario,
                 sintomas_presentes=solicitud_dto.sintomas_presentes,
                 emergencia_detectada=solicitud_dto.emergencia_detectada,
                 hospital = hospital,
-                triage=solicitud_dto.triage
+                triage= solicitud_dto.triage,
+                fecha_hora= fecha_hora
             )
 
             return JsonResponse({'mensaje': 'Solicitud creada exitosamente', 'id': solicitud.id}, status=200)
@@ -129,49 +149,70 @@ class SolicitudesView(View):
             return JsonResponse({'mensaje': 'El cuerpo de la solicitud no es JSON valido'}, status=400)
     
     def put(self, request):
-        data = {}
-        especifica = True
         try:
             data = json.loads(request.body)
         except json.decoder.JSONDecodeError:
-            especifica = False
-        
-        if especifica:
-            cambios = False
-            id = data.get('id')
-            
-            if id:
-                solicitud = self.buscar_por_id(id)
-            else:
-                return JsonResponse({'mensaje': 'Debe proporcionar un ID para una solicitud especifica.'}, status=400)
-            
+            return JsonResponse({'mensaje': 'El cuerpo de la solicitud no es JSON valido'}, status=400)
+
+        cambios = False
+        id = data.get('id')
+
+        if id:
+            solicitud = self.buscar_por_id(id)
             if solicitud:
-                latitud = data.get('latitud')
-                longitud = data.get('longitud')
-                usuario = data.get('usuario')
-                
-                
-                if latitud and solicitud.latitud != latitud:
-                    solicitud.latitud = latitud
+                direccion = data.get('direccion')
+                sintomas_presentes = data.get('sintomas_presentes')
+                emergencia_detectada = data.get('emergencia_detectada')
+                triage = data.get('triage')
+                id_usuario = data.get('id_usuario')
+                id_hospital = data.get('id_hospital')
+
+                if direccion is not None and solicitud.direccion != direccion:
+                    solicitud.direccion = direccion
                     cambios = True
-                elif longitud and solicitud.longitud != longitud:
-                    solicitud.longitud = longitud
+
+                if sintomas_presentes is not None:
+                    solicitud.sintomas_presentes = sintomas_presentes
                     cambios = True
-                elif usuario:
+
+                if emergencia_detectada is not None and solicitud.emergencia_detectada != emergencia_detectada:
+                    solicitud.emergencia_detectada = emergencia_detectada
+                    cambios = True
+
+                if triage is not None and solicitud.triage != triage:
+                    solicitud.triage = triage
+                    cambios = True
+
+                if id_usuario is not None:
                     try:
-                        usuario = Usuario.objects.get(id=usuario)
+                        usuario = Usuario.objects.get(id=id_usuario)
+                        if solicitud.usuario != usuario:
+                            if usuario.tipo_usuario.lower() != "usuario":
+                                return JsonResponse({'mensaje': 'No es un usuario valido para una solicitud (solo tipo usuario).'}, status=400)
+                            solicitud.usuario = usuario
+                            cambios = True
                     except Usuario.DoesNotExist:
-                        return JsonResponse({'mensaje': 'El usuario proporcionado no existe'}, status=404)
-                    if solicitud.usuario != usuario:
-                        solicitud.usuario = usuario
-                        cambios = True
+                        return JsonResponse({'mensaje': 'No existe un usuario con ese id en la base de datos.'}, status=404)
+
+                if id_hospital is not None:
+                    try:
+                        hospital = Hospital.objects.get(id=id_hospital)
+                        if solicitud.hospital != hospital:
+                            solicitud.hospital = hospital
+                            cambios = True
+                    except Hospital.DoesNotExist:
+                        return JsonResponse({'mensaje': 'No existe un usuario con ese id en la base de datos.'}, status=404)
+
                 if cambios:
                     solicitud.save()
                     return JsonResponse({'mensaje': 'Solicitud actualizada exitosamente'}, status=200)
                 else:
-                    return JsonResponse({'mensaje': 'No hubo cambios por realizar'}, status=400)
+                    return JsonResponse({'mensaje': 'No se realizaron cambios en la solicitud'}, status=200)
             else:
-                return JsonResponse({'mensaje': 'No se encontro una solicitud especifica.'}, status=400)
+                return JsonResponse({'mensaje': 'No se encontro una solicitud con el ID proporcionado'}, status=404)
+        else:
+            return JsonResponse({'mensaje': 'Debe proporcionar un ID para actualizar una solicitud'}, status=400)
+
     
     def delete(self, request):
         
@@ -188,13 +229,13 @@ class SolicitudesView(View):
             if id:
                 solicitud = self.buscar_por_id(id)
             else:
-                return JsonResponse({'mensaje': 'Parametros invalidos de busqueda.'}, status=400)
+                return JsonResponse({'mensaje': 'No hay una solicitud con ese id.'}, status=400)
             
             if solicitud:
                 solicitud.delete()
                 return JsonResponse({'mensaje': 'Solicitud eliminada exitosamente.'}, status=200)
             else:
-                return JsonResponse({'mensaje': 'No se encontro una solicitud con el parametro de busqueda enviado.'}, status=400)
+                return JsonResponse({'mensaje': 'No se pudo eliminar la solicitud.'}, status=400)
         else:
             return JsonResponse({'mensaje': 'Debe proporcionar un id para detectar la solicitud a eliminar.'}, status=400)
 
@@ -225,12 +266,16 @@ class IdentificadorView(View):
             sintomas = [sintoma for sintoma, respuesta in respuestasDTO.sintomas_presentes.items() if respuesta == "si"]
             puntaje = sum(sintoma['valor'] for sintoma in emergencia_identificada['sintomas'] if sintoma['sintoma'] in sintomas)
             triage = 0
-            if puntaje < 3:
-                triage = 1
-            elif puntaje < 7:
+            if puntaje <= 3:
+                triage = 5
+            elif puntaje <= 5:
+                triage = 4
+            elif puntaje <=7:
+                triage = 3
+            elif puntaje < 10:
                 triage = 2
             else:
-                triage = 3
+                triage = 1
             return JsonResponse({'emergencia_detectada': emergencia_identificada['nombre'], 'especialidad': emergencia_identificada['especialidad'], 'sintomas_presentes':sintomas, 'recomendaciones': emergencia_identificada['recomendaciones'], 'triage': triage}, status=200)
         else:
             return JsonResponse({'mensaje': 'No se pudo detectar la emergencia'}, status=200)
