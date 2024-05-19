6af688cd131c9b99812ac3d3d1dedd540150560f
Sebastian Sandoval <sebastian-sandoval@javeriana.edu.co>
2024-05-18 23:23:41 -0500

    actualizacion de settings

--- codigo_back/sigmed/settings.py
+++ codigo_back/sigmed/settings.py
@@ -121,3 +121,13 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
     "http://localhost:3002"
 ]
+
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
